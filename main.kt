f5b6906cd2fec530b563a7ed35670bc1f9ac0000
qlibcrist <bcrist@qualitylogic.com>
2020-10-28 13:58:44 -0600

    Created a tile class, which is used to represent tiles in the level class

--- main.kt
+++ main.kt
@@ -1,13 +1,44 @@
-class Level(level: CharArray) {
+class Tile(glyph: Char, enableCollision: Boolean) {
 
-    val level: CharArray = level
+    val glyph: Char = glyph
+
+    val enableCollision: Boolean = enableCollision
+
+}
+
+class Level(level: Array<Tile>, val x: Int, val y: Int) {
+
+    val level: Array<Tile> = level
+    val xLimit: Int = x
+    val yLimit: Int = y
+
+    fun printLevel() {
+        var c = 0
+        for (i in 0..level.size-1) {
+            if (c == x) { 
+                print("\n")
+                c = 0
+            }
+            print(" ${level[i].glyph} ")
+            c++ 
+        }
+    }
 
 }
 
 fun main() {
-    val levelArray = charArrayOf('a', 'b', 'c', 'd')
-    val level = Level(levelArray)
-    for (c in level.level) {
-        print(c)
-    }  
+    val grass = Tile('.', false)
+    val water = Tile('~', true)
+    val mountain = Tile('^', true)
+
+    val levelArray = arrayOf(
+        mountain, mountain, mountain, water, mountain,
+        mountain, grass, grass, water, mountain,
+        mountain, grass, grass, water, water,
+        grass, grass, grass, water, water,
+        grass, grass, grass, grass, water
+    )
+
+    val level = Level(levelArray, 5, 5)
+    level.printLevel()
 }
